4cc452fb29cb878804ca23da18c3972f4ec36841
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-04-04 19:05:21 +0530

    pulling from main

--- tracker-frontend/src/app/app.routes.ts
+++ tracker-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
+import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
+    {path: 'home',component:HomeComponent},
     {path : 'login',component:LoginComponent},
     {path : 'signup',component:SignupComponent}
 ];
